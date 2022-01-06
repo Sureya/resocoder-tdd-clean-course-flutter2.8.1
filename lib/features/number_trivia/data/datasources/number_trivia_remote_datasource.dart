3af39836e7260c4d09824e63c53a3b4e879ca690
Sureya Sathiamoorthi <ssathiamoorthi@expediagroup.com>
2022-01-06 16:41:24 +0000

    checkpoint at 4:27:47 chapter 9 completed

--- lib/features/number_trivia/data/datasources/number_trivia_remote_datasource.dart
+++ lib/features/number_trivia/data/datasources/number_trivia_remote_datasource.dart
@@ -1,4 +1,9 @@
+import 'dart:convert';
+
+import 'package:bloc_course/core/errors/exceptions.dart';
 import 'package:bloc_course/features/number_trivia/data/models/number_trivia_model.dart';
+import 'package:dio/dio.dart';
+
 
 abstract class NumberTriviaRemoteDataSource {
   /// Calls the number endpoint
@@ -10,4 +15,32 @@ abstract class NumberTriviaRemoteDataSource {
   ///
   /// Throws a [ServerException] for all error codes
   Future<NumberTriviaModel> getRandomNumberTrivia();
+}
+
+
+const NUMBERS_DOMAIN = "http://numbersapi.com" ;
+
+class NumberTriviaRemoteDataSourceImpl implements NumberTriviaRemoteDataSource {
+  final Dio dioClient;
+
+  NumberTriviaRemoteDataSourceImpl({required this.dioClient});
+
+  Future<NumberTriviaModel> _getNumberTriviaFromApi(String url) async {
+    final response = await dioClient.get(url);
+    if (response.statusCode == 200) {
+      return NumberTriviaModel.fromJson(json: jsonDecode(response.data));
+    } else {
+      throw ServerException();
+    }
+  }
+
+  @override
+  Future<NumberTriviaModel> getConcreteNumberTrivia(int number) async {
+    return _getNumberTriviaFromApi("$NUMBERS_DOMAIN/$number?json");
+  }
+
+  @override
+  Future<NumberTriviaModel> getRandomNumberTrivia() async {
+    return _getNumberTriviaFromApi("$NUMBERS_DOMAIN/random?json");
+  }
 }
---
+++ test/features/number_trivia/data/datasources/number_trivia_remote_data_source_test.dart
@@ -0,0 +1,142 @@
+import 'dart:convert';
+
+import 'package:bloc_course/core/errors/exceptions.dart';
+import 'package:bloc_course/features/number_trivia/data/datasources/number_trivia_remote_datasource.dart';
+import 'package:bloc_course/features/number_trivia/data/models/number_trivia_model.dart';
+import 'package:dio/dio.dart';
+import 'package:mocktail/mocktail.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+import '../../../../fixtures/fixture_reader.dart';
+
+class MockDioClient extends Mock implements Dio {}
+
+
+void main() {
+  late NumberTriviaRemoteDataSourceImpl dataSource ;
+  late MockDioClient mockDioClient ;
+
+  setUp((){
+    mockDioClient = MockDioClient() ;
+    dataSource = NumberTriviaRemoteDataSourceImpl(dioClient: mockDioClient);
+  });
+
+  void setUpMockHttpClientSuccess200() {
+    final responsePayload = fixture("trivia.json") ;
+    final httpResponse = Response(
+        data: responsePayload, requestOptions: RequestOptions(path: '/any'),
+        statusCode: 200
+    );
+
+    when(() => mockDioClient.get(any())).thenAnswer((_) async => httpResponse);
+  }
+  void setUpMockHttpClientFailure() {
+    when(() => mockDioClient.get(any())).thenThrow(ServerException());
+  }
+
+  group('getConcreteNumberTrivia', (){
+    final tNumber = 1 ;
+    final NumberTriviaModel tNumberTriviaModel = NumberTriviaModel.fromJson(json: jsonDecode(fixture("trivia.json")));
+
+     test('''should perform a GET request on a URL with number being on the 
+            endpoint and with application/json header''',
+     () async {
+      // arrange
+       setUpMockHttpClientSuccess200() ;
+
+      // act
+       await dataSource.getConcreteNumberTrivia(tNumber);
+
+      // assert
+       verify(
+       () => mockDioClient.get(
+           "$NUMBERS_DOMAIN/$tNumber?json",
+       )).called(1);
+    });
+
+    test('''should perform a GET request and return number trivia''',
+    () async {
+          // arrange
+          setUpMockHttpClientSuccess200() ;
+
+          // act
+          final result = await dataSource.getConcreteNumberTrivia(tNumber);
+
+          // assert
+          verify(
+                  () => mockDioClient.get("$NUMBERS_DOMAIN/$tNumber?json")
+          ).called(1);
+
+          expect(result, equals(tNumberTriviaModel));
+
+        });
+
+    test('''should perform a GET request and return ServerException on failure''',
+
+    () async {
+          // arrange
+          setUpMockHttpClientFailure();
+
+          // act
+          final call = dataSource.getConcreteNumberTrivia;
+
+          // assert
+          expect( () => call(tNumber), throwsA(TypeMatcher<ServerException>()));
+
+        });
+
+
+  });
+
+  group('getRandomNumberTrivia', (){
+    final NumberTriviaModel tNumberTriviaModel = NumberTriviaModel.fromJson(json: jsonDecode(fixture("trivia.json")));
+
+    test('''should perform a GET request on a URL with number being on the 
+            endpoint and with application/json header''',
+            () async {
+          // arrange
+          setUpMockHttpClientSuccess200() ;
+
+          // act
+          await dataSource.getRandomNumberTrivia();
+
+          // assert
+          verify(
+                  () => mockDioClient.get("$NUMBERS_DOMAIN/random?json")
+          ).called(1);
+        });
+
+    test('''should perform a GET request and return number trivia''',
+            () async {
+          // arrange
+          setUpMockHttpClientSuccess200() ;
+
+          // act
+          final result = await dataSource.getRandomNumberTrivia();
+
+          // assert
+          verify(
+                  () => mockDioClient.get("$NUMBERS_DOMAIN/random?json")
+          ).called(1);
+
+          expect(result, equals(tNumberTriviaModel));
+
+        });
+
+    test('''should perform a GET request and return ServerException on failure''',
+
+            () async {
+          // arrange
+          setUpMockHttpClientFailure();
+
+          // act
+          final call = dataSource.getRandomNumberTrivia;
+
+          // assert
+          expect( () => call(), throwsA(TypeMatcher<ServerException>()));
+
+        });
+
+
+  });
+}

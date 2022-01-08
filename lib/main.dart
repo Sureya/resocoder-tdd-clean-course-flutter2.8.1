e6731ae25e85d16e89eb077493beea873e232c08
Sureya Sathiamoorthi <catchsureya@gmail.com>
2022-01-08 23:55:43 +0000

    checkpoint at 06:29:24 Chapter 13 completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,11 @@
+import 'package:bloc_course/injection_container.dart' as di;
 import 'package:flutter/material.dart';
 
-void main() => runApp(const MyApp());
+void main() async {
+  await di.init();
+  runApp(const MyApp());
+}
+
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);

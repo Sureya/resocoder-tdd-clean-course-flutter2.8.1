cbd28924b88be86dbad32b044adff7146c4c24c0
Sureya Sathiamoorthi <catchsureya@gmail.com>
2022-01-10 09:47:57 +0000

    ISSUE: bloc not working

--- lib/features/number_trivia/presentation/pages/number_trivia_page.dart
+++ lib/features/number_trivia/presentation/pages/number_trivia_page.dart
@@ -2,68 +2,62 @@ import 'package:bloc_course/features/number_trivia/presentation/bloc/number_triv
 import 'package:bloc_course/injection_container.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-
-final greetingProvider = Provider((ref) => 'Start Searching') ;
-
-class NumberTriviaPage extends ConsumerWidget {
+class NumberTriviaPage extends StatelessWidget {
   NumberTriviaPage({required Key key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
+  Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Number Trivia'),
-      ),
-      body: buildBody(
-          mainContext:context,
-          widgetRef: ref
-        )
+        appBar: AppBar(
+          title: Text('Number Trivia'),
+        ),
+        body: buildBody(context:context)
     );
   }
 
-  Widget buildBody({
-    required BuildContext mainContext,
-    required WidgetRef widgetRef
-  }){
+  BlocProvider buildBody({required BuildContext context}){
+    return BlocProvider(
+      create: (context) => sl<NumberTriviaBloc>(),
+      child: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(10.0),
+          child: Column(
+            children: [
+              SizedBox(height: 20.0,),
+              BlocBuilder<NumberTriviaBloc, NumberTriviaState>(
+                  builder: (context, state) {
+                    print(MediaQuery.of(context).size.height/2.5);
+                    print("*********** $state") ;
+                    if (state is Empty) {
+                      return Text("1");
+                    }else{
+                      return Text("2");
+                    }
+                  }
+              ),
+              Container(
+                  margin: EdgeInsets.all(5),
+                  height: MediaQuery.of(context).size.height/2.5,
+                  child: Placeholder()
+              ),
+              SizedBox(height: 20.0,),
+              Column(
+                children: [
+                  Placeholder(fallbackHeight: 50),
+                  SizedBox(height: 25),
+                  Row(
+                    children: [
+                      Expanded(child: Placeholder(fallbackHeight: 30)),
+                      SizedBox(width: 10),
+                      Expanded(child: Placeholder(fallbackHeight: 30),),
+                    ],
+                  )
+                ],
+              )
 
-    final greeting = widgetRef.watch(greetingProvider);
-
-    return Center(
-      child: Padding(
-        padding: const EdgeInsets.all(10.0),
-        child: Column(
-          children: [
-            SizedBox(height: 20.0,),
-            Container(
-                margin: EdgeInsets.all(5),
-                height: MediaQuery.of(mainContext).size.height/3,
-                child: Text(
-                    greeting,
-                    style: TextStyle(
-                      fontSize: 25,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.black54
-                    ),
-                )
-            ),
-            SizedBox(height: 20.0,),
-            Column(
-              children: [
-                Placeholder(fallbackHeight: 50),
-                SizedBox(height: 25),
-                Row(
-                  children: [
-                    Expanded(child: Placeholder(fallbackHeight: 30)),
-                    SizedBox(width: 10),
-                    Expanded(child: Placeholder(fallbackHeight: 30),),
-                  ],
-                )
-              ],
-            )
-
-          ],
+            ],
+          ),
         ),
       ),
     ) ;
